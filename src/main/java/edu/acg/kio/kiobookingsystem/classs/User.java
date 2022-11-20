376fede47ea93c1b0851d9a9780a483d40292f79
PcOOl1311 <kostas13vr1@gmail.com>
2022-11-20 19:33:05 +0200

    Constructor/Getter/Setter + toString function for User Class

--- src/main/java/edu/acg/kio/kiobookingsystem/classs/User.java
+++ src/main/java/edu/acg/kio/kiobookingsystem/classs/User.java
@@ -11,9 +11,56 @@ public class User {
     private UserType userType;
 
 
+    // CONSTRUCTOR
+    public User(String name, String password, String contactInfo, UserType userType) {
+        this.name = name;
+        this.password = password;
+        this.contactInfo = contactInfo;
+        this.userType = userType;
+    }
 
+    // GETTER
 
+    public String getName() {
+        return name;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public String getContactInfo() {
+        return contactInfo;
+    }
 
+    public UserType getUserType() {
+        return userType;
+    }
+
+    // SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public String toString() {
+        return "------------------------------------" + " \n" +
+                " Name     = " + name + " \n" +
+                " Password = " + password + " \n" +
+                " Contact Info:  = " + contactInfo + " \n" +
+                " User Type  = " + userType + " \n" +
+                "------------------------------------";
+    }
 }
