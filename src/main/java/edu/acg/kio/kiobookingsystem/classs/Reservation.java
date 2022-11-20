17b9f75f3598d927089ac4f06ac4ad96596abd1e
giolena <g.kokorou@acg.edu>
2022-11-20 18:56:48 +0200

    added reservation class

---
+++ src/main/java/edu/acg/kio/kiobookingsystem/classs/Reservation.java
@@ -0,0 +1,10 @@
+package edu.acg.kio.kiobookingsystem.classs;
+
+import edu.acg.kio.kiobookingsystem.enumerators.TimeSlot;
+
+public class Reservation {
+    private User user;
+    private Table table;
+    private TimeSlot time;
+    private String day; //TODO find a type for date
+}
